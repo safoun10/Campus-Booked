54d8ad7cb6216dfd1bf6f5e13cf27a09d2873538
Safoun <safoun.official@gmail.com>
2023-07-23 17:04:18 +0600

    banner added

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
+import "./TopNav.css"
+import { Link } from "react-router-dom";
 
 const TopNav = () => {
-    return (
-        <div>
-            TopNav
-        </div>
-    );
+	return (
+		<div className="d-flex justify-content-between align-items-center py-3 px-5 topnav">
+			<div className="d-flex gap-3">
+				<img
+					className="logo"
+					src="https://i.postimg.cc/CxvZVMRL/school.png"
+					alt="logo"
+				/>
+				<div className="display-6 text-two">
+					<span className="camp">CAMP</span>US
+					<br /> BOOKED
+				</div>
+			</div>
+			<div className="d-flex justify-content-between align-items-center gap-5">
+				<div className="d-flex fs-4 gap-3 align-items-center">
+					<Link className="text-decoration-none text-white active">
+						Home
+					</Link>
+					<Link className="text-decoration-none text-white">
+						Colleges
+					</Link>
+					<Link className="text-decoration-none text-white">
+						Admission
+					</Link>
+					<Link className="text-decoration-none text-white">
+						My College
+					</Link>
+				</div>
+				<div>
+					<Link className="text-decoration-none btn login-btn">
+						Login
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default TopNav;
--- src/layout/home/Home.jsx
+++ src/layout/home/Home.jsx
@@ -1,13 +1,76 @@
-import React from 'react';
-import TopNav from '../../components/topnav/TopNav';
+import React from "react";
+import "./Home.css";
+import TopNav from "../../components/topnav/TopNav";
+import { Link } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
+import { Tilt } from "react-tilt";
+
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+
+const defaultOptions = {
+	reverse: false, // reverse the tilt direction
+	max: 15, // max tilt rotation (degrees)
+	perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+	scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+	speed: 1000, // Speed of the enter/exit transition
+	transition: true, // Set a transition on enter/exit.
+	axis: null, // What axis should be disabled. Can be X or Y.
+	reset: true, // If the tilt effect has to be reset on exit.
+	easing: "cubic-bezier(.03,.98,.52,.99)", // Easing on enter/exit.
+};
 
 const Home = () => {
-    return (
-        <div>
-            <TopNav></TopNav>
-            Home
-        </div>
-    );
+	return (
+		<div>
+			<section className="banner">
+				<TopNav></TopNav>
+				<div className="d-flex px-5 align-items-center">
+					<div>
+						<div className="banner-text">
+							Dream Big, <br /> Explore Bigger! <br /> Book Your
+							College Experience with Us!
+						</div>
+						<Link className="text-decoration-none">
+							<div className="explore d-flex align-items-center gap-3">
+								<span>Explore Our Colleges </span>
+								<span>
+									<BsArrowRight className="fs-4"></BsArrowRight>
+								</span>
+							</div>
+						</Link>
+					</div>
+					<div className="banner-card me-5 mt-3 d-flex flex-column justify-content-center align-items-center">
+						<Tilt
+							options={defaultOptions}
+							style={{ height: 380, width: 400 }}
+						>
+							<div className="d-flex flex-column justify-content-center p-5 align-items-center">
+								<div>
+									<Player
+										autoplay
+										loop
+										src="https://lottie.host/05dfdb83-e32b-4a37-82ca-18f50acd9550/dzqMt2iI4V.json"
+										style={{
+											height: "250px",
+											width: "420px",
+										}}
+									></Player>
+								</div>
+								<div className="fs-5 text-center fw-bold">
+									Fire Up Your College Adventure 😃
+								</div>
+							</div>
+						</Tilt>
+						<div className="btn w-100 bg-admit mt-4">
+							<Link className="text-decoration-none text-white">
+								Admit Now !
+							</Link>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 };
 
 export default Home;
